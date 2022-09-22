06d3e0e7f0cbc6793fda1d9a41c472964b4c760a
BarkMulcher <lucas.c.haskell@gmail.com>
2022-09-22 18:27:16 -0400

    edit schema pk/fk

--- schema.sql
+++ schema.sql
@@ -13,8 +13,9 @@ CREATE TABLE role (
     id INT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN KEY(department_id),
+    department_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id),
     REFERENCES department(id),
     ON DELETE SET NULL
 );
@@ -25,7 +26,7 @@ CREATE TABLE employee (
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
-    FOREIGN KEY(role_id),
+    FOREIGN KEY (role_id),
     REFERENCES role(id),
     ON DELETE SET NULL
-)
+);
--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const inquirer = require('inquirer');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 
+async function main() {
+    // get client
+    const mysql = require('mysql2/promise');
+    // create cx
+    const connection = await mysql.createConnection({
+        host: 'localhost', 
+        user: 'root', 
+        password: 'poop',
+        database: 'employees_db'
+    });
+    // query db
+    const [rows, fields] = await connection.execute('SELECT * FROM `table` WHERE `name` = ? AND `age` > ?', ['Morty', 14]);
+}
 
 const questions = () => {
     return inquirer.prompt ([
@@ -21,7 +40,37 @@ const questions = () => {
 }
 
 const init = () => {
+    main();
     questions();
 }
 
 init();
+
+
+
+// const mysql = require('mysql2');
+
+// // create connection to database
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'test'
+// });
+
+// // simple query
+// connection.query(
+//     'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+//     function(err, results, fields) {
+//         console.log(results); // results contains rows returned by server
+//         console.log(fields); // fields contains extra metadata about results, if present
+//     }
+// );
+
+// // with placeholder
+// connection.query(
+//     'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+//     ['Page', 45],
+//     function(err, results) {
+//         console.log(results);
+//     }
+// );
